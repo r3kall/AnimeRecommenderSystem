f6b358e4c3207c224b37dcd6261c403be4c58adc
r3kall <lnz.rutigliano@gmail.com>
2017-01-05 14:28:38 +0100

    first commit

---
+++ animerecommendersystem/definitions.py
@@ -0,0 +1,13 @@
+
+import os
+
+# Directories
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is the Project Root
+
+FILE_DIR = os.path.join(ROOT_DIR, 'files')  # This is the directory of stored files
+
+if not os.path.exists(FILE_DIR):
+    os.makedirs(FILE_DIR)
+
+
+# Files
