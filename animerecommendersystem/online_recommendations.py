b69b4911ead233458e6b8f702ff0177d0f4f320d
r3kall <lnz.rutigliano@gmail.com>
2017-02-14 14:45:46 +0100

    online recommendations

--- animerecommendersystem/online_recommendations.py
+++ animerecommendersystem/online_recommendations.py
@@ -10,40 +10,31 @@ from user_cluster_matrix import read_user_item_json, build_user_cluster_matrix
 from item_cluster_matrix import build_item_feature_matrix, item_cluster_matrix
 
 
-def get_recommendations(user, user_item_matrix, neighbors):
+def get_recommendations(user_item_matrix, neighbors, username):
 
-    rec_list = []
-    for item_id, attributes in user_item_matrix[user]['list'].iteritems():
-        if attributes['rate'] == 0:
-            pred_num = 0.
-            pred_dem = 0.
+    rec_dict = {}
+    # find possible reccomandations in the user neighborhood
+    for n in neighbors:
+        for item_id, attributes in user_item_matrix[n[0]]['list'].iteritems():
+            # consider an item only if has positive rate
+            if attributes['rate'] >= 6:
+                if rec_dict.get(item_id) is None:
+                    rec_dict[item_id] = {}
+                    rec_dict[item_id]['ranksum'] = float(attributes['rate']) * n[1]
+                    rec_dict[item_id]['simsum'] = n[1]
+                else:
+                    rec_dict[item_id]['ranksum'] += float(attributes['rate']) * n[1]
+                    rec_dict[item_id]['simsum'] += n[1]
 
-            for n in neighbors[user]:
-                neigh_rate = user_item_matrix[n[0]]['list'].get(item_id)
-                if neigh_rate is not None:
-                    vrate = neigh_rate['rate']
-                    if vrate > 0:
-                        pred_num += n[1] * (
-                            vrate - user_item_matrix[n[0]]['mean_rate']
-                        )
-                        pred_dem += n[1]
+    # remove items already in the user list
+    user_list = user_item_matrix[username]
+    for item_id in user_list['list'].iterkeys():
+        if item_id in rec_dict.keys():
+            rec_dict.pop(item_id)
 
-            if pred_dem == 0:
-                # print "\nNo item in neigh"
-                # prediction = user_item_matrix[user]['mean_rate']
-                continue
-            else:
-                prediction = user_item_matrix[user]['mean_rate'] + (
-                pred_num / pred_dem)
-
-            if prediction < 1.:
-                prediction = 1.
-            if prediction > 10.:
-                prediction = 10.
-
-            rec_list.append((item_id, prediction))
-    rec_list = sorted(rec_list, key=lambda x: x[1], reverse=True)
-    return rec_list
+    # sort per weighted mean
+    rec_list = [(k, rec_dict[k]['ranksum'] / rec_dict[k]['simsum']) for k in rec_dict.keys()]
+    return sorted(rec_list, key=lambda x: x[1], reverse=True)
 
 
 def input_loop(c, k, n):
@@ -55,6 +46,7 @@ def input_loop(c, k, n):
     """
     # these are the same for all iterations
     user_item = read_user_item_json(definitions.JSON_USER_FILE)
+    print "[ Username example:  %s ]" % user_item.keys()[123]
     item_feature, pos_to_id, id_to_pos = build_item_feature_matrix()
     item_cluster = item_cluster_matrix(item_feature, c)
 
@@ -67,9 +59,9 @@ def input_loop(c, k, n):
                          k)
 
     username = str(raw_input('Insert username: '))
-    r = get_recommendations(username, user_item, neighs)
+    r = get_recommendations(user_item, neighs[username], username)
     print r[:n]
 
 
 if __name__ == '__main__':
-    input_loop(10, 10, 10)
+    input_loop(61, 20, 30)
